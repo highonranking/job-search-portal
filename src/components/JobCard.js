cfde110b358cdc5d07da39a79257c8c3041a3722
Abhinav Dixit <abhinavdixit@192.168.1.2>
2024-05-05 14:31:21 +0530

    feat: Job card

--- src/components/JobCard.js
+++ src/components/JobCard.js
@@ -1,30 +1,60 @@
-import React from 'react';
-import { Grid, Card, CardContent, Typography, Button } from '@mui/material';
+import React, {useState} from 'react';
+import { Card,
+    CardContent,
+    Typography,
+    Button,
+    Grid,
+    Chip, } from '@mui/material';
 
-const JobCard = () => {
+const JobCard = ({job}) => {
+    const [expanded, setExpanded] = useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
   return (
     <Card>
-        <CardContent>
-            <Grid container spacing={4}>
-                <Grid item>
-                    <img src="https://pngimg.com/uploads/bruce_lee/bruce_lee_PNG44.png"/>
-                </Grid>
-                <Grid item xs={12} sm>
-                    <div>
-                        <Typography variant='h5'>frontend</Typography>
-                        <Typography variant='body1'>Weekday</Typography>
-                        <Typography variant='body1'>Bengaluru</Typography>
-                        <Typography variant='body2'>Job Description</Typography>
-                        <Button>read more</Button>
-                    </div>
-                </Grid>
-            </Grid>
-            <div>
-                <Typography variant='body1'>1-3 years experience</Typography>
-                <Button>Apply Now</Button>
+      <CardContent>
+        <Grid container spacing={4}>
+          <Grid item>
+            <img
+              src={job.logoUrl}
+              alt="Company Logo"
+              className="company-logo"
+            />
+          </Grid>
+          <Grid item xs={12} sm>
+            <div className="info-container">
+              <Typography variant="h5">{job.jobRole}</Typography>
+              <Typography variant="body1">
+                Company: {job.companyName}
+              </Typography>
+              <Typography variant="body1">Location: {job.location}</Typography>
+              <Typography variant="body2" className="description">
+                Description:{" "}
+                {job.jobDetailsFromCompany
+                  ? expanded
+                    ? job.jobDetailsFromCompany
+                    : `${job.jobDetailsFromCompany.substring(0, 100)}...`
+                  : "No description available"}
+                <Button onClick={handleExpandClick} className="expand-button">
+                  {expanded ? "Read less" : "Read more"}
+                </Button>
+              </Typography>
             </div>
-        </CardContent>
+          </Grid>
+        </Grid>
+        <div className="info-container">
+          <Typography variant="body1" className="experience">
+            Experience Required: {job.minExp} years to {job.maxExp} years
+          </Typography>
+          <Button variant="contained" color="primary" className="apply-button">
+            Apply
+          </Button>
+        </div>
+      </CardContent>
     </Card>
+
   )
 }
 
