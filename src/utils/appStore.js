337c91f7295a5be5c20e489eb21544d43acb5168
Abhinav Dixit <abhinavdixit@192.168.1.2>
2024-05-05 13:28:34 +0530

    configuring redux store

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,1 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import jobReducer from './features/jobs/jobSlice';
+
+
+export default configureStore({
+    reducer: {
+      jobs: jobReducer,
+    },
+  });
+  
